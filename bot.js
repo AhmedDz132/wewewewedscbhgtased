1323b3fb3741a724ed29ac77a963b28196ee01ed
AhmedDz132 <41522322+AhmedDz132@users.noreply.github.com>
2018-07-21 21:04:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,3 +49,17 @@ let welcomer = member.guild.channels.find("name","اسم روم الترحيب")
  
       }
       });
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "~say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
